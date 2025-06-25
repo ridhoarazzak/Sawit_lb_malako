83ab64f8803cd5c8408320a506f6c99549520f15
ridhoarazzak <54082062+ridhoarazzak@users.noreply.github.com>
2025-06-25 10:34:26 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SawitMap from './components/SawitMap';
+
+function App() {
+  return (
+    <div className="App">
+      <SawitMap />
+    </div>
+  );
+}
+
+export default App;
