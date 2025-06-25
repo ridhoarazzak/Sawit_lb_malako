1c28ff58e2ee503c61f6e3e38604dd25b1056ed9
ridhoarazzak <54082062+ridhoarazzak@users.noreply.github.com>
2025-06-25 12:57:04 +0700

    Update app.js
    Commit

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SawitMap from './components/SawitMap';
-import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Navigate to="/peta" />} />
         <Route path="/peta" element={<SawitMap />} />
       </Routes>
     </Router>
