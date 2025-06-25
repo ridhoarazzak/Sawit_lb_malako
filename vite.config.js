8012976b8422686e6c50b700787115b56efa3dbd
ridhoarazzak <54082062+ridhoarazzak@users.noreply.github.com>
2025-06-25 13:57:41 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true }
+  server: {
+    host: true
+  },
+  build: {
+    outDir: 'dist'
+  }
 });
